1a84317a5735c8cdca7005d73841b2819e23f3b5
chaimaej <cjarid@SMB.SQLI.COM>
2022-03-14 17:26:24 +0100

    uses of ANT

--- Animal.java
+++ Animal.java
@@ -1,26 +1,9 @@
 public class Animal {
 
-    public static String getDefaultGreeting(){
-        return "Hello";
+    public static void sayHello(){
+        System.out.println("hello");
     }
-
-    public static String getCatGreeting(){
-        return "Meow";
-    }
-    public static String getBirdGreeting(){
-        return "bird";
-    }
-
-
     public static void main(String[] args) {
-        String value = args[0];
-        if (value.equalsIgnoreCase("cat"))
-            System.out.println(getCatGreeting());
-<<<<<<< HEAD
-        
-=======
-        else if(value.equalsIgnoreCase("Bird"))
-            System.out.println(getBirdGreeting());
->>>>>>> bird
+       sayHello();
     }
 }
