cea4e5bbbdbc53270272194ecafbe802d15fac61
chaimaej <cjarid@SMB.SQLI.COM>
2022-03-14 14:40:46 +0100

    add new file Animal.java

---
+++ Animal.java
@@ -0,0 +1,16 @@
+public class Animal {
+
+    public static String getDefaultGreeting(){
+        return "Hello";
+    }
+
+    public static String getCatGreeting(){
+        return "Meow";
+    }
+
+    public static void main(String[] args) {
+        String value = args[0];
+        if (value.equalsIgnoreCase("cat"))
+            System.out.println(getCatGreeting());
+    }
+}
